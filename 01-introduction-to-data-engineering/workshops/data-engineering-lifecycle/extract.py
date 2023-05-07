965de312e14626d87208db1e0a198bab35af9f2b
Topp <wind13wind@gmail.com>
2023-05-07 03:00:57 +0000

    Update extract file

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/extract.py
@@ -4,8 +4,10 @@ import requests
 
 
 if __name__ == "__main__":
-    url = ""
+    url = "https://dog.ceo/api/breeds/image/random"
     response = requests.get(url)
     data = response.json()
 
     # Your code here
+    with open('dogs.json', 'w' ) as f:
+        json.dump(data, f)
